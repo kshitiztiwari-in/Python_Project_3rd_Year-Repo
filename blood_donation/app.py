44bc6affb739d7bfeecc7565999c89ef2f11a2be
kshitiztiwari-in <kshitiztiwari.in@gmail.com>
2025-11-19 19:07:17 +0530

    Register Page Completed just need to connect it with Database

--- blood_donation/app.py
+++ blood_donation/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from routes.home import home_bp
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'blood_donation_web_application'
+
+mysql = MySQL(app)
 
 # REGISTER BLUEPRINTS
 app.register_blueprint(home_bp)
--- blood_donation/routes/home.py
+++ blood_donation/routes/home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+
 home_bp = Blueprint("home_bp", __name__)
 
 @home_bp.route("/")
